a5d1d347bdfbb0cfcf598b63dda67d7276d1e008
stephband <stephband@stephband.local>
2010-03-31 01:44:47 +0200

    Implements a homing algorithm rather than a one-at-a-time letter remover, cutting the number of DOM tests by a factor of about 10. Adds truncate-by-word, and minor optimisations

--- js/jquery.truncate.js
+++ js/jquery.truncate.js
@@ -1,51 +1,55 @@
 // jquery.truncate.js
 // 
-// 0.4
+// 0.5
 // Stephen Band
 // 
 // Truncates the html of a node so that the node is no greater than one line high
 // Options:
-// marker					- character or jQuery object plcaed at end of truncation.
+// marker					- character or jQuery object placed at end of truncation.
 // match 					- content to match in height, typically smething like '<br /><br />'.
-// truncateByWord - causes truncation only to happen on word boundaries. Faster.
+// truncateByWord - causes truncation only to happen on word boundaries.
 //
 // TODO:
-// Try using letter-spacing to condense words before truncating them
 // Support file extensions by truncating the middle a la OS X: 'xxxxxx...xxx'
 
 (function(undefined){
 	
 	var options = {
-				marker: '&hellip;',
-				match: '&nbsp;',
+				marker: '\u2026',
 				truncateByWord: false
-				//height: false
 			},
-			// Uses a zero width character for testing
-			testNode = jQuery('<span/>').html('&#8204;');
+			// Uses a zero width space for testing
+			testNode = jQuery('<span/>').html('\u200B');
 	
-	jQuery.fn.truncate = function(text, o){
+	function fillNode(node, html, testNode){
+		node
+		.html(
+			jQuery( document.createTextNode( html ) )
+			.add( testNode )
+		);
+	}
+	
+	jQuery.fn.truncate = function(text, opt){
 		if (typeof text !== 'string') {
-			o = text;
+			opt = text;
 			text = undefined;
 		}
 		
-		o = jQuery.extend({}, options, o);
+		o = jQuery.extend({}, options, opt);
 		
 		return this.each(function(){
 			var node = jQuery(this),
 					data = node.data('truncate'),
 					html = text || data && data.html || node.html(),
+					match = o.match || data && data.match || '&nbsp;',
 					newHtml = html,
-					testHeight = o.height,
 					length = html.length,
-					lineHeight = parseInt( node.css('lineHeight') ),
-					targetOffset, normalOffset, currentOffset,
-					lineOffset, lines;
+					targetOffset, normalOffset, currentOffset, x;
 			
 			if ( !data ) {
 				node.data('truncate', {
-					html: html
+					html: html,
+					match: match
 				});
 				
 				jQuery(window)
@@ -60,67 +64,52 @@
 			 return;
 			}
 			
-			// Offset of the node when it's full,
+			// Offset of the node when it has target content.
+			node
+			.html( match )
+			.append( testNode );
+			targetOffset = testNode.offset();
+			
+			// Offset of the node when it's full.
 			node
 			.html( html )
 			.append( testNode );
 			currentOffset = testNode.offset();
 			
-			// and when it has target content in it.
-			node
-			.html( o.match )
-			.append( testNode );
-			targetOffset = testNode.offset();
-			
-			// Why go on if everything's already hunkydory?
+			// Why carry on if everything's already hunkydory?
 			if ( currentOffset.top <= targetOffset.top ) {
-				node.html( html );
+				testNode.remove();
 				return;
 			};
 			
-			// Put the content back in and get offset of testNode
-			// Normal offset is important when the node is inside something
-			// that's floated. A full node could be causing the float to clear
-			// another element, and perhaps that's why we're truncating it.
-			node.append( html );
-			normalOffset = testNode.offset();
+			x = length;
 			
-			// If browser has not given us a useful lineHeight, calculate it
-			if ( !lineHeight ) {
-				testNode.before( '<br />' );
-				lineOffset = testNode.offset();
-				lineHeight = lineOffset.top - normalOffset.top;
-			}
-			
-			// Lop.
-			// Crude, but effective. When the text is big, roughly lop off the chunk
-			// we don't need. 'm's are wider than 'i's so we mustn't be too brutal
-			// with the lopping-off factor. 3 is a good.
-			if ( currentOffset.top !== normalOffset.top ) { 
-			  lines = Math.ceil( (currentOffset.top - normalOffset.top) / lineHeight );
-			  length = Math.ceil( length / lines ) * 3;
-			}
-			
-			// Iterate.
-			// Reduce length by letter
-			while ( length-- && currentOffset.top > targetOffset.top ) {
+			// Home in.
+			while ( x > 1 || currentOffset.top > targetOffset.top ) {
+				x = Math.ceil(x/2);
 				
-				// Reduce length by word
-				if ( o.truncateByWord ) {
-					
-					// TODO: make this test regex for non-word characters
-					while ( length-- && html[ length ] !== ' ' ) {}
-				}
+				length = currentOffset.top > targetOffset.top ?
+					length - x :
+					length + x ;
 				
 				newHtml = html.slice(0, length) + o.marker;
-				node
-				.html( newHtml )
-				.append( testNode );
-				
+				fillNode(node, newHtml, testNode);
 				currentOffset = testNode.offset();
 			}
 			
+			// Lop off letters until we find a space.
+			if ( o.truncateByWord ) {
+				while ( length-- ) {
+					if ( /\s/.test( html[length+1] ) ) {
+						newHtml = html.slice(0, length+1) + o.marker;
+				  		fillNode(node, newHtml, testNode);
+						break;
+					}
+				}
+			}
+			
 			testNode.remove();
+			return;
 		});
 	};
 })();
